2dfb302894147133054cf1f4a0655c1a051f632e
AlexCarnerooo <alexandrecarnerop@gmail.com>
2025-04-03 15:36:21 +0200

    Fix GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/alexandrecarnero.github.io',
-  assetPrefix: '/alexandrecarnero.github.io/',
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig; 
